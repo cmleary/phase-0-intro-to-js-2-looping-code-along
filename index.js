edf2c1e0c7b2e5829a9a8332f7be517af837ecfb
Chris Leary <learymchris@gmail.com>
2023-07-28 11:55:31 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+let names = [];
+let day = "";
+let messages = [];
+function writeCards(names, day) {
+  for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${day} gift!`);
+    
+  }
+
+  return messages;
+}
+
+function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number --;
+    }
+  }
